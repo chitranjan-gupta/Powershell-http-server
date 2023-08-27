7b3ba4c8e9efb2de9249482690d4c2fa12130a64
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2023-08-27 17:38:42 +0530

    Initial commit

---
+++ index.ps1
@@ -0,0 +1,23 @@
+$port = 8000
+$root = "E:\Ref\ngrok"
+
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+
+Write-Host "Listening on port $port"
+
+while($listener.IsListening){
+    $context = $listener.GetContext()
+    $response = $context.Response
+    $path = $root + $context.Request.RawUrl
+    if(Test-Path -Path $path -PathType Leaf){
+        $content = Get-Content -Path $path -Raw
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+        $response.ContentLength64 = $buffer.Length
+        $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    } else {
+        $response.StatusCode = 404
+    }
+    $response.Close()
+}
